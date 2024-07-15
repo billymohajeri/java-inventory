5ddddca6f975d0bf65bd7b62ec4180799fb07e8e
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-15 23:43:40 +0300

    Add constructor for item class

--- src/inventory/Item.java
+++ src/inventory/Item.java
@@ -1,9 +1,27 @@
 package inventory;
 
-import java.time.LocalDate;
+import java.util.Date;
 
 public class Item {
-  private final String _name;
+  private final String NAME;
   private int quantity;
-  private LocalDate _createdDate;
+  private Date createdDate;
+
+  Item(String name, int quantity) {
+    this.NAME = name;
+    if (quantity < 0) {
+      throw new IllegalArgumentException("Amount of each item cannot be negative");
+    }
+    this.quantity = quantity;
+    this.createdDate = new Date();
+  }
+
+  Item(String name, int quantity, Date createdDate) {
+    this.NAME = name;
+    if (quantity < 0) {
+      throw new IllegalArgumentException("Amount of each item cannot be negative");
+    }
+    this.quantity = quantity;
+    this.createdDate = createdDate;
+  }
 }
