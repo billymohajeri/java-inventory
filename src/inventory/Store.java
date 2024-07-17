0cb00b71f44ab459a7a057f0c483a356d12b67e2
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-17 22:19:00 +0300

    Add addItem method

--- src/inventory/Store.java
+++ src/inventory/Store.java
@@ -29,7 +29,6 @@ public class Store {
     return itemsQuantityMap;
   }
 
-
   public Set<String> getUniqueItemNames() {
     Set<String> uniqueItemNames = new HashSet<>();
     for (Item item : items) {
@@ -37,6 +36,19 @@ public class Store {
     }
     return uniqueItemNames;
   }
+//   Methods to add/delete one item to the collection. Do not allow adding items with the same name to the store.
+
+  public boolean addItem(Item newItem) {
+    for (Item item : items) {
+      if (item.getName().equals(newItem.getName())) {
+        return false;
+      }
+    }
+    this.items.add(newItem);
+    return true;
+  }
+
+
 }
 
 
